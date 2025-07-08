f133507c007a3c825e6753cdc874d4175d0deaf0
Duy2408BT <ltkduy2408@gmail.com>
2025-07-08 11:39:28 +0700

    SVGLine.h

--- SVGDemo/SVGDemo/SVGLine.h
+++ SVGDemo/SVGDemo/SVGLine.h
@@ -1,27 +1,28 @@
-#define SVGLine_H
 #ifndef SVGLine_h
+#define SVGLine_h
+
 #include <windows.h>
 #include <objidl.h>
 #include <gdiplus.h>
+#include <vector>
 #include "Point.h"
 #include "SVGElement.h"
 
-class SVGLine : public SVGElement
+class SVGPolygon : public SVGElement
 {
 private:
-	svg::Point center;
-	float radiusX1;
-	float radiusY1;
-	float radiusX2;
-	float radiusY2;
-	Gdiplus::Color strokeColor;
-	float strokeWidth;
+    std::vector<svg::Point> points;
+    Gdiplus::Color fillColor;
+    Gdiplus::Color strokeColor;
+    float strokeWidth;
+
 public:
-	SVGLine(const svg::Point& center, float radiusX1, float radiusY1,
-		float radiusX2, float radiusY2,
-		Gdiplus::Color strokeColor, float strokeWidth);
+    SVGPolygon(const std::vector<svg::Point>& points,
+        Gdiplus::Color fillColor,
+        Gdiplus::Color strokeColor,
+        float strokeWidth);
 
-
-	void render(Gdiplus::Graphics* graphics) override;	
+    void render(Gdiplus::Graphics* graphics) override;
 };
+
 #endif
