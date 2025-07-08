2ee7d1574fdb6a5d3fac4e249cc5f9096e09a2b1
Duy2408BT <ltkduy2408@gmail.com>
2025-07-08 11:40:06 +0700

    SVGLine.cpp

--- SVGDemo/SVGDemo/SVGLine.cpp
+++ SVGDemo/SVGDemo/SVGLine.cpp
@@ -1,28 +1,24 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 #include "SVGLine.h"
-#include "Point.h"
-#include "cmath"
 
 using namespace Gdiplus;
 
-SVGLine::SVGLine(const svg::Point& center, float radiusX1, float radiusY1, float radiusX2, float radiusY2, Gdiplus::Color strokeColor, float strokeWidth)
-	: center(center), radiusX1(radiusX1), radiusY1(radiusY1), radiusX2(radiusX2), radiusY2(radiusY2), strokeColor(strokeColor), strokeWidth(strokeWidth) {}
+SVGPolygon::SVGPolygon(const std::vector<svg::Point>& points,
+    Gdiplus::Color fillColor,
+    Gdiplus::Color strokeColor,
+    float strokeWidth)
+    : points(points), fillColor(fillColor), strokeColor(strokeColor), strokeWidth(strokeWidth) {}
 
-void SVGLine::render(Gdiplus::Graphics* graphics) {
-	Gdiplus::Pen pen(strokeColor, strokeWidth);
-    Gdiplus::SolidBrush brush(Color(100, 200, 200, 255));
+void SVGPolygon::render(Gdiplus::Graphics* graphics) {
+    if (points.size() < 3) return;
 
-    Gdiplus::REAL x1 = center.x + radiusX1;
-    Gdiplus::REAL y1 = center.y + radiusY1;
-    Gdiplus::REAL x2 = center.x + radiusX2;
-    Gdiplus::REAL y2 = center.y + radiusY2;
+    std::vector<Gdiplus::PointF> gdiPoints;
+    for (const auto& p : points)
+        gdiPoints.emplace_back(Gdiplus::PointF(p.x, p.y));
 
-    // Tính toạ độ và kích thước hình chữ nhật từ 2 điểm bất kỳ
-    Gdiplus::REAL left = min(x1, x2);
-    Gdiplus::REAL top = min(y1, y2);
-    Gdiplus::REAL width = abs(x2 - x1);
-    Gdiplus::REAL height = abs(y2 - y1);
+    Gdiplus::SolidBrush brush(fillColor);
+    Gdiplus::Pen pen(strokeColor, strokeWidth);
 
-    graphics->FillRectangle(&brush, left, top, width, height);
-    graphics->DrawRectangle(&pen, left, top, width, height);
+    graphics->FillPolygon(&brush, gdiPoints.data(), gdiPoints.size());
+    graphics->DrawPolygon(&pen, gdiPoints.data(), gdiPoints.size());
 }
