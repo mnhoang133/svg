5a32832af6b8be41a9bf7f39c7e27a2b629bfe21
Duy2408BT <ltkduy2408@gmail.com>
2025-08-14 16:53:26 +0700

    SVGPathParser.cpp

--- SVGDemo/SVGDemo/SVGPathParser.cpp
+++ SVGDemo/SVGDemo/SVGPathParser.cpp
@@ -8,6 +8,7 @@
 #include "ParserUtils.h"
 #include "SVGAttributeUtils.h"
 #include <cstdlib>
+#include "SVGGradientParser.h"
 
 using namespace ParserUtils;
 using namespace AttributeParserUtils;
@@ -24,10 +25,21 @@ SVGElement* SVGPathParser::parse(const std::string& line) const
     float fillOpacity = safeParseFloat(fillOpStr, 1.0f);
     float strokeOpacity = safeParseFloat(strokeOpStr, 1.0f);
 
-    Color fill = fillStr.empty() || fillStr == "none" ? Color(0, 0, 0, 0) : applyOpacity(parseColor(fillStr), fillOpacity);
+    Color fill = Color(0, 0, 0, 0);
+    std::string fillUrl;
+
+    if (!fillStr.empty() && fillStr != "none") {
+        if (SVGGradientParser::isFillGradientUrl(fillStr)) {
+            fillUrl = fillStr;
+        }
+        else {
+            fill = applyOpacity(parseColor(fillStr), fillOpacity);
+        }
+    }
     Color stroke = strokeStr.empty() || strokeStr == "none" ? Color(0, 0, 0, 0) : applyOpacity(parseColor(strokeStr), strokeOpacity);
 
     auto* path = new SVGPath(std::wstring(d.begin(), d.end()), fill, stroke);
+    path->setFillUrl(fillUrl);
 
     std::string transformStr = extractAttr(line, "transform");
     if (!transformStr.empty())
